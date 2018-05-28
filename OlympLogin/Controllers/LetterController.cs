be9a055b4c49629e72944cd0f9d19f05f46a2691
Александр Белёв <belyov97@gmail.com>
2018-05-28 05:10:53 +0500

    letter prepare

---
+++ OlympLogin/Controllers/LetterController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OlympLogin.Models;
+using OlympLogin.ViewModels;
+
+namespace OlympLogin.Controllers
+{
+    [Authorize(Roles="admin")]
+    public class LetterController : Controller
+    {
+        private readonly kladrContext _context;
+
+        public LetterController(kladrContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(string ids)
+        {
+            ViewData["ids"] = ids;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Send(LetterVIewModel model)
+        {
+            
+        }
+    }
+}
